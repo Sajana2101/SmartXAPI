a81be1729756ee2183da105afa8f04fd8be7c612
Sajana2101 <ST10433940@vcconnect.edu.za>
2026-09-07 20:53:50 +0200

    Add generic telemetry packet and custom buffer

---
+++ SmartX.Api/Models/TelementryBuffer.cs
@@ -0,0 +1,33 @@
+﻿namespace SmartX.Api.Models
+{
+    public class TelemetryBuffer<T>
+    {
+        private readonly List<TelemetryPacket<T>> _items = new();
+
+        public int Count => _items.Count;
+
+        public void Add(TelemetryPacket<T> packet)
+        {
+            _items.Add(packet);
+        }
+
+        public IReadOnlyList<TelemetryPacket<T>> GetAll()
+        {
+            return _items.AsReadOnly();
+        }
+
+        public IReadOnlyList<TelemetryPacket<T>> GetBySensor(Guid sensorId)
+        {
+            return _items
+                .Where(packet => packet.SensorId == sensorId)
+                .OrderByDescending(packet => packet.TimestampUtc)
+                .ToList()
+                .AsReadOnly();
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+    }
+}
---
+++ SmartX.Api/Models/TelemetryPacket.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartX.Api.Models
+{
+    public class TelemetryPacket<T>
+    {
+        public Guid Id { get; set; }
+
+        public Guid SensorId { get; set; }
+
+        public string Metric { get; set; } = string.Empty;
+
+        public T Value { get; set; } = default!;
+
+        public DateTime TimestampUtc { get; set; }
+    }
+}
