271f6a2a1db25b538f74810ba66aa207410e5621
Sajana2101 <ST10433940@vcconnect.edu.za>
2026-09-08 17:09:48 +0200

    Configure secure sensor attachment storage

---
+++ SmartX.Api/Options/AttachmentOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace SmartX.Api.Options
+{
+    public class AttachmentOptions
+    {
+        public const string SectionName = "Attachments";
+
+        public string StoragePath { get; set; } =
+            "App_Data/Attachments";
+
+        public long MaxFileSizeBytes { get; set; } =
+            10 * 1024 * 1024;
+
+        public string[] AllowedExtensions { get; set; } =
+            Array.Empty<string>();
+
+        public string EncryptionKey { get; set; } =
+            string.Empty;
+    }
+}
